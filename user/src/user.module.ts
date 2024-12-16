c895882011ea9c20b656087c1a6bcd677745aa19
Milad Taghizadeh <taghizadehmilad1382@gmail.com>
2024-12-16 19:04:01 +0330

    feat: user schema and user module created and connected to mongodb database

---
+++ user/src/user.module.ts
@@ -0,0 +1,20 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UserService } from './user.service';
+import { UserController } from './user.controller';
+import { User, UserSchema } from './schema/user.schema';
+
+@Module({
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost:27017/microservice-todo'),
+    MongooseModule.forFeature([
+      {
+        name: User.name,
+        schema: UserSchema,
+      },
+    ]),
+  ],
+  controllers: [UserController],
+  providers: [UserService],
+})
+export class UserModule {}

c452fa67970c2884a3818f0b1d69a8973ac4b502
Milad Taghizadeh <taghizadehmilad1382@gmail.com>
2024-12-16 19:03:31 +0330

    feat : user microservice created

--- user/src/main.ts
+++ user/src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { UserModule } from './user.module';
+import { TcpOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const app = await NestFactory.createMicroservice(UserModule, {
+    transport: Transport.TCP,
+    options: {
+      host: '0.0.0.0',
+      port: 4001,
+    },
+  } as TcpOptions);
+  await app.listen();
+  console.log('userService: http://localhost:4001');
 }
 bootstrap();

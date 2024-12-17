c5b20872e4eb3bd5d5d0550d0a254e780be3f71f
Milad Taghizadeh <taghizadehmilad1382@gmail.com>
2024-12-17 22:26:32 +0330

    feat: user extended for logging data on microservice for test

--- user/src/user.controller.ts
+++ user/src/user.controller.ts
@@ -1,12 +1,22 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller } from '@nestjs/common';
 import { UserService } from './user.service';
+import { MessagePattern } from '@nestjs/microservices';
+import { IFindById, ILogin, ISignup } from './interface/user.interface';
 
-@Controller()
+@Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
-  @Get()
-  getHello(): string {
-    return this.userService.getHello();
+  @MessagePattern('signup')
+  signup(signupDto: ISignup) {
+    return this.userService.signup(signupDto);
+  }
+  @MessagePattern('login')
+  login(loginDto: ILogin) {
+    return this.userService.login(loginDto);
+  }
+  @MessagePattern('get_by_id')
+  getUserById(findDto: IFindById) {
+    return this.userService.getById(findDto);
   }
 }
--- user/src/user.service.ts
+++ user/src/user.service.ts
@@ -2,11 +2,18 @@ import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { User, UserDocument } from './schema/user.schema';
 import { Model } from 'mongoose';
+import { IFindById, ILogin, ISignup } from './interface/user.interface';
 
 @Injectable()
 export class UserService {
   constructor(@InjectModel(User.name) private userModel: Model<UserDocument>) {}
-  getHello(): string {
-    return 'Hello World!';
+  signup(signupDto: ISignup) {
+    console.log(signupDto);
+  }
+  login(loginDto: ILogin) {
+    console.log(loginDto);
+  }
+  getById(findDto: IFindById) {
+    console.log(findDto);
   }
 }
